42c7329a8f1829970f6f216045a1247dce1c7b6e
ElvinM95 <emustafayev064@gmail.com>
2026-09-22 18:10:50 +0400

    'Task02.03'

---
+++ 02. C#_Lessons/Lesson03/Tasks/CsharpTask02/CsharpTask02/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace CsharpTask02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 01
+
+            /* verilmish 4 reqemli ededin reqemlerinin cemini tap */
+            //    int number;
+
+            //    Console.WriteLine("Enter a 4-digit number:");
+            //l1:
+            //    if (!int.TryParse(Console.ReadLine(), out number))
+            //    {
+            //        Console.WriteLine("Invalid input. Please enter a valid 4-digit number.");
+            //        goto l1;
+            //    }
+
+            //    if (number < 1000 || number > 9999)
+            //    {
+            //        Console.WriteLine("Invalid input. Please enter a valid 4-digit number.");
+            //        goto l1;
+            //    }
+
+            //    int sum = 0;
+
+            //    while (number > 0)
+            //    {
+            //        int newNum = number % 10;
+            //        sum += newNum;
+            //        number /= 10;
+            //    }
+
+            //    Console.WriteLine("The sum of the digits is: " + sum);
+
+            #endregion
+
+            #region Task 02
+
+            /* verilmihs 6 reqemli ededin ilk 3 denesinin reqemleri cemi tap: example: 123600= 1+2+3 */
+
+            //    int number;
+
+            //    Console.WriteLine("Enter a 6-digit number:");
+            //l1:
+            //    if (!int.TryParse(Console.ReadLine(), out number))
+            //    {
+            //        Console.WriteLine("Invalid input. Please enter a valid 6-digit number.");
+            //        goto l1;
+            //    }
+
+            //    if (number < 100000 || number > 999999)
+            //    {
+            //        Console.WriteLine("Invalid input. Please enter a valid 6-digit number.");
+            //        goto l1;
+            //    }
+
+            //    int sum = 0;
+
+            //    int firstThreeDigits = number / 1000; // Get the first three digits
+
+            //    while (firstThreeDigits > 0)
+            //    {
+            //        int newNum = firstThreeDigits % 10;
+            //        sum += newNum;
+            //        firstThreeDigits /= 10;
+            //    }
+
+            //    Console.WriteLine("The sum of the first three digits is: " + sum);
+
+            #endregion
+
+            #region Task 03
+
+            /* verilmihs 9 reqemli ededin duz ortaya dushen 3 reqeminin reqemleri cemi */
+
+        //    int number;
+        //    Console.WriteLine("Enter a 9-digit number:");
+        //l1:
+        //    if (!int.TryParse(Console.ReadLine(), out number))
+        //    {
+        //        Console.WriteLine("Invalid input. Please enter a valid 9-digit number.");
+        //        goto l1;
+        //    }
+        //    if (number < 100000000 || number > 999999999)
+        //    {
+        //        Console.WriteLine("Invalid input. Please enter a valid 9-digit number.");
+        //        goto l1;
+        //    }
+
+        //    int sum = 0;
+        //    int middleThreeDigits = (number / 1000) % 1000; // Get the middle three digits
+        //    Console.WriteLine("The middle three digits are: " + middleThreeDigits);
+        //    while (middleThreeDigits > 0)
+        //    {
+        //        int newNum = middleThreeDigits % 10;
+        //        sum += newNum;
+        //        middleThreeDigits /= 10;
+        //    }
+        //    Console.WriteLine("The sum of the middle three digits is: " + sum);
+
+            #endregion
+        }
+    }
+}
